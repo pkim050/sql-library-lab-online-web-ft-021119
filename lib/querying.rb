27ab9a8a232db67b706fcd8f08ad3f3658f1983d
Patrick Kim <pkim050@gmail.com>
2019-03-20 05:50:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,7 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "Write your SQL query here"
-  "SELECT characters.speciesCOUNT(characters.species) FROM characters GROUP BY characters.species LIMIT 1"
+  "SELECT characters.species, COUNT(characters.species) FROM characters GROUP BY characters.species LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
